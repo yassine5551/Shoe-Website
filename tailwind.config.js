070bb9f82a5d29c35264afc11b662cdae4a827fe
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-22 17:52:43 +0000

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        light: "#6d6d6f",
+        main_dark:"#3d3d3f",
+        dark:"#2e3845",
+        color1: "#ff656d",
+        color2:"#fbaa72"
+      }
+    },
+  },
+  plugins: [],
+}
+
