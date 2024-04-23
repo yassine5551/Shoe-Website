c4432e53513c2b598fa68cbb13bd33043fc0e94d
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-23 21:47:18 +0000

    adding animation using npm aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  })
   return (
     <>
-   
+
       <Navbar />
       <Hero />
      
